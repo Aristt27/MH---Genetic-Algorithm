5fa05d59e2fe2e02f37addc3e5e7ed52d5222588
Bruno Lima <37422849+Aristt27@users.noreply.github.com>
2021-01-15 22:38:14 -0300

    Add files via upload

--- Genetic_iterator.py
+++ Genetic_iterator.py
@@ -6,18 +6,27 @@ def genetic_iterator(Instancia, set_params, set_stop_criteria, Iterations, outpu
     ans = []
     tts = []
     its = []
+    winners = []
+    bestsol = 9999999999999
     for _ in range(Iterations):
         t0 = time()
         Evo, Evo_Scores =  Genetic_Algorithm(Instancia, set_params, set_stop_criteria, Verbose = False)
         tf = time() - t0
         al = len(Evo)
         sx = Evo_Scores[-1][-1]
+        if sx < bestsol:
+            bestsol = sx
+            winners = Evo[-1][-1]
         its.append(al)
         tts.append(tf)
         ans.append(sx)
         if output:
             with open(output, 'a') as file:
                 file.write(str(sx)+','+str(tf)+','+str(al) + '\n')
+            with open("solution/" + output[:-4]+'sol.csv', 'w') as file2:
+                file2.write("Cirurgia (c),Sala (r),Dia (d),Horário (t)\n")
+                for wdx, col in enumerate(winners):
+                    file2.write(str(wdx+1)+', '+str(col[:-1])[1:-1]+', '+str(col[-1]+1) + '\n')
 
     if output:
         
